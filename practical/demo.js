c64753391619f7fe47e0d64e1edbd3848b444dff
Tom Pies <tombo@Toms-MacBook-Pro.local>
2022-06-04 15:05:46 +0100

    added conditional search

--- practical/demo.js
+++ practical/demo.js
@@ -1,19 +1,50 @@
 var infoContainer = document.getElementById("info");
 var btn = document.getElementById("btn");
 
+function textSearch() {
+  textInput = document.getElementById("myText").value;
+  textInput = textInput.replace(" ", "+");
+  console.log("search text" + textInput);
+}
+
+function searchType() {
+  searchTypeInput = document.getElementById("searchTypeInput").value;
+  console.log(searchTypeInput);
+}
+
 btn.addEventListener("click", function () {
   var ourRequest = new XMLHttpRequest();
   ourRequest.open(
     "GET",
-    "http://openlibrary.org/search.json?q=the+lord+of+the+rings&page=1&limit=10"
+    "http://openlibrary.org/search.json?q=" + textInput + "&page=1&limit=10"
   );
   ourRequest.onload = function () {
     var ourData = JSON.parse(ourRequest.responseText);
     console.log(ourData.docs);
+
     renderHTML(ourData.docs);
   };
   ourRequest.send();
 });
+
+// } else {
+//   btn.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open(
+//       "GET",
+//       "https://openlibrary.org/api/books?bibkeys=ISBN:" +
+//         isbnInput +
+//         "&jscmd=data&format=json"
+//     );
+//     ourRequest.onload = function () {
+//       var ourData = JSON.parse(ourRequest.responseText);
+//       console.log(ourData.docs);
+
+//       renderHTML(ourData.docs);
+//     };
+//     ourRequest.send();
+//   });
+// }
 
 function renderHTML(data) {
   var htmlString = data;
